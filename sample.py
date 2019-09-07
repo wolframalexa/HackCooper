b823ad9697d0ff40c59234453bdc4f7724104327
wolframalexa <alexa.jakob4@gmail.com>
2019-09-06 22:56:06 -0400

    sample.py

---
+++ sample.py
@@ -0,0 +1,29 @@
+import plotly.graph_objects as go
+
+mapbox_access_token = open(".mapbox_token").read()
+
+fig = go.Figure(go.Scattermapbox(
+        lat=['45.5017'],
+        lon=['-73.5673'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=14
+        ),
+        text=['Montreal'],
+    ))
+
+fig.update_layout(
+    hovermode='closest',
+    mapbox=go.layout.Mapbox(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=go.layout.mapbox.Center(
+            lat=45,
+            lon=-73
+        ),
+        pitch=0,
+        zoom=5
+    )
+)
+
+fig.show()
