935edf5c03cbd050b89a88a8726b006c6e84ccc1
wolframalexa <alexa.jakob4@gmail.com>
2019-09-07 00:43:55 -0400

    updates

--- map.py
+++ map.py
@@ -10,13 +10,15 @@ fig = go.Figure(go.Scattermapbox(
 	text = []
 	file = open("High-School-Data.csv", "r")
 	reader = csv.reader(file)
-	for i in range(0, 477):
-		#READ LATITUDES AND LONGITUDES		        
-		lat.append(#latitude)
-		lon.append(#longitude)
-		text.append(#HS Name)
+	for idx,line in reader:
+		if idx>0:
+			#READ LATITUDES AND LONGITUDES		        
+			lat.append(line[9])
+			lon.append(line[10])
 	mode='markers',
 	marker=go.scattermapbox.Marker(size=9),
+	for line in reader:
+		text.append(line[2])
 ))
 
 fig.update_layout(
@@ -26,8 +28,8 @@ fig.update_layout(
         accesstoken=mapbox_access_token,
         bearing=0,
         center=go.layout.mapbox.Center(
-            lat=38.92,
-            lon=-77.07
+            lat=40.73
+            lon=-73.96
         ),
         pitch=0,
         zoom=10
