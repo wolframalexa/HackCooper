634dd2d0b4ad47ca9669189509748493d499b665
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-09-07 09:55:46 -0400

    ok lets begin

--- divsch/src/component/map.js
+++ divsch/src/component/map.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, Dot,ButtonNext,DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class map extends React.Component {
   render() {
     return (<div>
-            <iframe src="https://nycplot.herokuapp.com/"  height="600" width="100%" ></iframe>
+       <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        totalSlides={4}
+        orientation	={"horizontal"}
+      >
+
+        <Slider>
+          <Slide index={0}>  
+          <iframe src="https://nycplot.herokuapp.com/"  height="600" width="100%" ></iframe>
+          </Slide>
+          <Slide index={1}>
+          <iframe src="//plot.ly/~ajakob/7.embed"  height="600" width="100%" ></iframe>
+
+          </Slide>
+          <Slide index={2}>
+          <iframe src="//plot.ly/~ajakob/6.embed"  height="600" width="100%" ></iframe>
+
+          </Slide>
+          <Slide index={3}>
+          <iframe src="//plot.ly/~ajakob/5.embed"  height="600" width="100%" ></iframe>
+          </Slide>
+        </Slider>
+        <ButtonBack class="mapbutton" id="back">&#x27F5;</ButtonBack>
+        <ButtonNext  class="mapbutton" id="next">&#x27F6;</ButtonNext>
+      </CarouselProvider>
+           
         </div>
         );
     }
