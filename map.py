ad8f389fc3fc3ed52a32c6a61004b003f48785f2
wolframalexa <alexa.jakob4@gmail.com>
2019-09-07 01:15:04 -0400

    message

--- map.py
+++ map.py
@@ -3,20 +3,17 @@ import pandas as pd
 
 mapbox_access_token = open(".mapbox_token").read()
 
-text = []
-#file = open("High-School-Data.csv", "r")
-#reader = csv.reader(file)
 data = pd.read_csv("High-School-Data.csv")
 lats = data["Latitute"].tolist()
 longs = data["Longitude"].tolist()
+names = data["School Name"].tolist()
 
 fig = go.Figure(go.Scattermapbox(
         lat = lats,
         lon = longs,
         mode='markers',
         marker=go.scattermapbox.Marker(size=9),
-        for line in reader:
-                text.append(line[2])
+        text = names,
 ))
 
 fig.update_layout(
@@ -26,8 +23,8 @@ fig.update_layout(
         accesstoken=mapbox_access_token,
         bearing=0,
         center=go.layout.mapbox.Center(
-            lat=40.73
-            lon=-73.96
+            lat=40.73,
+            lon=-73.96,
         ),
         pitch=0,
         zoom=10
