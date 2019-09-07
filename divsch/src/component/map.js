4eb9e4dacd5ca23f2984fdb09e5f72ee15813286
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-09-07 03:23:09 -0400

    map sorta works lul

--- divsch/src/component/map.js
+++ divsch/src/component/map.js
@@ -3,7 +3,7 @@ import React from 'react';
 class map extends React.Component {
   render() {
     return (<div>
-            <iframe src="./NYCPlot.html"></iframe>
+            <iframe src="https://nycplot.herokuapp.com/"  height="600" width="100%" ></iframe>
         </div>
         );
     }
