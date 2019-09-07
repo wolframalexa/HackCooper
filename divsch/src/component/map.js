b002e539eeda443efe559dc178a31dc770075b37
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-09-07 01:47:31 -0400

    prettier nao

--- divsch/src/component/map.js
+++ divsch/src/component/map.js
@@ -4,7 +4,7 @@ class map extends React.Component {
   render() {
     return (
       <div id="mapmain">
-        <iframe src="https://plot.ly/~ajakob/2/"></iframe>
+        <iframe id="iframe" src="https://plot.ly/~ajakob/2/"></iframe>
       </div>
     );
   }
